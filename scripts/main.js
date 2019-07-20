d0d5db9fb5e052118773c060f27b800a3e6d6ac1
Jim <jlgriffith180@gmail.com>
2019-07-20 14:08:56 -0500

    Major overhaul to experience and portfolio display

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,21 @@ $(document).ready(function () {
   $('.header').height($(window).height());
 })
 
+function animateCSS(element, animationName, callback) {
+  var nodeList = document.querySelectorAll(element);
+  for (var i = 0; i < nodeList.length; ++i) {
+    nodeList[i].classList.add('animated', animationName);
+  }
+
+  function endAnimation() {
+    for (var j = 0; j < nodeList.length; ++j) {
+      nodeList[j].classList.remove('animated', animationName)
+      nodeList[j].removeEventListener('animationend', endAnimation)
+      if (typeof callback === 'function') callback()
+    }
+  }
+
+  for (var k = 0; k < nodeList.length; ++k){
+    nodeList[k].addEventListener('animationend', endAnimation)
+  }
+}
