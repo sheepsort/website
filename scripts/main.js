10d5955f2fe32d242fff084020fba0c67a1a4619
Jim <jlgriffith180@gmail.com>
2019-07-21 01:50:52 -0500

    animations for tech icons & projects

--- scripts/main.js
+++ scripts/main.js
@@ -20,3 +20,21 @@ function animateCSS(element, animationName, callback) {
     nodeList[k].addEventListener('animationend', endAnimation)
   }
 }
+function applyGrayscale(keepsColor) {
+  var colorlessElements = document.querySelectorAll('.tech-icon');
+
+  for (var i=0; i < colorlessElements.length; ++i){
+    if (!colorlessElements[i].classList.contains(keepsColor)){
+      colorlessElements[i].style.WebkitFilter="grayscale(100%)";
+      colorlessElements[i].style.opacity="0.50";
+    } 
+  }
+  setTimeout(function removeGrayscale(){
+    for (var i=0; i < colorlessElements.length; ++i){
+      colorlessElements[i].style.WebkitFilter="grayscale(0%)";
+      colorlessElements[i].style.opacity="1";
+    }
+  }, 7000);
+}
+
+
