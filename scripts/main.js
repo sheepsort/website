36da46905e46aa6336c06371b0ff8b4bb1551013
Jim <jlgriffith180@gmail.com>
2019-07-22 12:00:46 -0500

    fixed animations - not pretty, but they work again.

--- scripts/main.js
+++ scripts/main.js
@@ -10,12 +10,52 @@ $(document).ready(function() {
 //   AnimateIcon(".javascriptIcon", "shake");
 // });
 
-// const AnimateCSS = (element, animationName, callback) => {
+const AnimateCSS = (element, animationName, callback) => {
+  var nodeList = document.querySelectorAll(element);
+  for (var i = 0; i < nodeList.length; ++i) {
+    nodeList[i].classList.add("animated", animationName);
+  }
+  
+  function endAnimation() {
+    for (var j = 0; j < nodeList.length; ++j) {
+      nodeList[j].classList.remove("animated", animationName);
+      nodeList[j].removeEventListener("animationend", endAnimation);
+      if (typeof callback === "function") callback();
+    }
+  }
+  
+  for (var k = 0; k < nodeList.length; ++k) {
+    nodeList[k].addEventListener("animationend", endAnimation);
+  }
+}
+
+// document.getElementById("jsButton").addEventListener("click", function() {
+  //   animateCSS(".javascriptIcon", "shake");
+  // });
+  
+  const ApplyGrayscale = keepsColor => {
+    var colorlessElements = document.querySelectorAll(".tech-icon");
+    
+    for (var i = 0; i < colorlessElements.length; ++i) {
+      if (!colorlessElements[i].classList.contains(keepsColor)) {
+        colorlessElements[i].style.WebkitFilter = "grayscale(100%)";
+        colorlessElements[i].style.opacity = "0.50";
+      }
+    }
+    setTimeout(function removeGrayscale() {
+      for (var i = 0; i < colorlessElements.length; ++i) {
+        colorlessElements[i].style.WebkitFilter = "grayscale(0%)";
+        colorlessElements[i].style.opacity = "1";
+      }
+    }, 7000);
+  };
+  
+// function AnimateCSS(element, animationName, callback) {
 //   var nodeList = document.querySelectorAll(element);
 //   for (var i = 0; i < nodeList.length; ++i) {
 //     nodeList[i].classList.add("animated", animationName);
 //   }
-  
+
 //   function endAnimation() {
 //     for (var j = 0; j < nodeList.length; ++j) {
 //       nodeList[j].classList.remove("animated", animationName);
@@ -23,51 +63,11 @@ $(document).ready(function() {
 //       if (typeof callback === "function") callback();
 //     }
 //   }
-  
+
 //   for (var k = 0; k < nodeList.length; ++k) {
 //     nodeList[k].addEventListener("animationend", endAnimation);
 //   }
 // }
-
-// document.getElementById("jsButton").addEventListener("click", function() {
-  //   animateCSS(".javascriptIcon", "shake");
-  // });
-  
-  // const ApplyGrayscale = keepsColor => {
-  //   var colorlessElements = document.querySelectorAll(".tech-icon");
-    
-  //   for (var i = 0; i < colorlessElements.length; ++i) {
-  //     if (!colorlessElements[i].classList.contains(keepsColor)) {
-  //       colorlessElements[i].style.WebkitFilter = "grayscale(100%)";
-  //       colorlessElements[i].style.opacity = "0.50";
-  //     }
-  //   }
-  //   setTimeout(function removeGrayscale() {
-  //     for (var i = 0; i < colorlessElements.length; ++i) {
-  //       colorlessElements[i].style.WebkitFilter = "grayscale(0%)";
-  //       colorlessElements[i].style.opacity = "1";
-  //     }
-  //   }, 7000);
-  // };
-  
-  // function animateCSS(element, animationName, callback) {
-  //   var nodeList = document.querySelectorAll(element);
-  //   for (var i = 0; i < nodeList.length; ++i) {
-  //     nodeList[i].classList.add("animated", animationName);
-  //   }
-  
-  //   function endAnimation() {
-  //     for (var j = 0; j < nodeList.length; ++j) {
-  //       nodeList[j].classList.remove("animated", animationName);
-  //       nodeList[j].removeEventListener("animationend", endAnimation);
-  //       if (typeof callback === "function") callback();
-  //     }
-  //   }
-  
-  //   for (var k = 0; k < nodeList.length; ++k) {
-  //     nodeList[k].addEventListener("animationend", endAnimation);
-  //   }
-  // }
 // document.getElementById("jsButton").addEventListener("click", function() {
 //   ApplyGrayscale("javascriptIcon");
 // });
